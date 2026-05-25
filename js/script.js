2663507b44b0f4760461187a7bbf0cde69bffe38
akariitlab <groupit.akari@gmail.com>
2026-05-25 15:58:37 -0500

    Se ajustaron cambios en la web

--- js/script.js
+++ js/script.js
@@ -34,13 +34,43 @@ if(navMenu && navCloseBtn){
     })
 }
 // Change header bg color
+window.addEventListener("scroll", () => {
+    const scrollY = window.pageYOffset;
 
+    if(scrollY > 5){
+        document.querySelector("header").classList.add("header-active");
+    }else{
+        document.querySelector("header").classList.remove("header-active");
+    }
+})
 
 // Scroll up button
   
-  
+  const scrollUpBtn = document.querySelector(".scrollUp-btn");
+  if (scrollY >  250){
+    scrollUpBtn.classList.add("scrollUpBtn-active");
+  }else{
+    scrollUpBtn.classList.remove("scrollUpBtn-active");
+  }
   
 // Nav link indicator
+    const sections = document.querySelectorAll('section[id]');
+
+    sections.forEach(section =>{
+        const sectionHeight = section.offsetHeight,
+            sectionTop = section.offsetTop - 60;
+
+        let navId = document.querySelector(`.menu-content a[href*=${section.id}]`);
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            navId.classList.add("active-navlink")
+        }else {
+            navId.classList.remove("active-navlink")
+        }
+
+    })
+
+console.log(sections)
  
   
 // Scroll Reveal Animation
